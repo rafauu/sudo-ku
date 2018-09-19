53e1a3dfdaefc5ba370d50201f7839dcd2e88bae
rafauu <gliniakr@gmail.com>
2018-09-19 19:30:06 +0200

    refactor + class responsibilities changed

--- main.cpp
+++ main.cpp
@@ -5,158 +5,133 @@
 #include <cctype>
 #include <numeric>
 #include <set>
+#include <algorithm>
+#include <iomanip> // setw
 #include <boost/range/adaptors.hpp>
 #include <boost/range/irange.hpp>
- 
+
 template <typename T>
 class FlattenVector
 {
 public:
-	FlattenVector<T>(size_t rows,
-	                 size_t cols)
-	: rows{rows}
-	, cols{cols}
-	{ vec.reserve(rows*cols); };
-	T& operator[](size_t index) { return vec[index]; }
-	const T& operator[](size_t index) const { return vec[index]; }
+	FlattenVector<T>(uint rows, uint cols)
+	: rows{rows}, cols{cols}
+	{
+	    vec.reserve(rows*cols);
+	};
+	
+	T& operator[](uint index) { return vec[index]; }
+	const T& operator[](uint index) const { return vec[index]; }
 	void emplace_back(T&& element) { vec.emplace_back(std::move(element)); }
-	bool checkNthRowCorrectness(size_t row)
+	size_t size() const { return vec.size(); }
+	std::vector<T> getSpecificPart(const auto& predicate)
 	{
-	    std::vector<size_t> indexes(rows*cols);
+	    std::vector<uint> indexes(vec.size());
 	    std::iota(indexes.begin(), indexes.end(), 0);
- 
-	    auto nthRow = [this, row](size_t index){ return index/3 == row; };//9
-	    auto indexToValueFromVector = [this](size_t index){ return vec[index]; };
-	    //auto hasValue = [this](T element){ return bool(element.value); };
- 
+	    auto indexToValueFromVector = [this](uint index){ return vec[index]; };
 	    using namespace boost::adaptors;
-        auto temp = boost::copy_range<std::vector<T>>(indexes | filtered(nthRow)
-                                                              | transformed(indexToValueFromVector));
-                                                              //| filtered(hasValue));
- 
-        return temp.size() == std::set<T>(temp.begin(), temp.end()).size();
+	    return boost::copy_range<std::vector<T>>(indexes | filtered(predicate)
+                                                         | transformed(indexToValueFromVector));
 	}
- 
-	bool checkNthColCorrectness(size_t row)
-	{
-	    std::vector<size_t> indexes(rows*cols);
-	    std::iota(indexes.begin(), indexes.end(), 0);
- 
-	    auto nthRow = [this, row](size_t index){ return index%3 == row; };//9
-	    auto indexToValueFromVector = [this](size_t index){ return vec[index]; };
-	    //auto hasValue = [this](T element){ return bool(element.value); };
- 
-	    using namespace boost::adaptors;
-        auto temp = boost::copy_range<std::vector<T>>(indexes | filtered(nthRow)
-                                                              | transformed(indexToValueFromVector));
-                                                              //| filtered(hasValue));
- 
-        return temp.size() == std::set<T>(temp.begin(), temp.end()).size();
-	}
- 
-	bool checkNthSqrCorrectness(size_t row)
-	{
-	    std::vector<size_t> indexes(rows*cols);
-	    std::iota(indexes.begin(), indexes.end(), 0);
- 
-	    auto nthRow = [this, row](size_t index){ return index<9; };//9
-	    auto indexToValueFromVector = [this](size_t index){ return vec[index]; };
-	    //auto hasValue = [this](T element){ return bool(element.value); };
- 
-	    using namespace boost::adaptors;
-        auto temp = boost::copy_range<std::vector<T>>(indexes | filtered(nthRow)
-                                                              | transformed(indexToValueFromVector));
-                                                              //| filtered(hasValue));
- 
-        display();
-        return temp.size() == std::set<T>(temp.begin(), temp.end()).size();
-	}
- 
 	void display() const
 	{
 	    using namespace boost::adaptors;
- 
 	    for(const auto& el : vec | indexed(0))
 	    {
 	        if(el.index() % cols == 0) std::cout << std::endl;
-	        std::cout << el.value();
+	        std::cout << std::setw(2) << el.value() << " "; //remove setw and iomanip
 	    }
+	    std::cout << std::endl;//remove
 	}
+	
+	const uint rows;
+    const uint cols;
+    
 private:
-    size_t rows;
-    size_t cols;
 	std::vector<T> vec;
 };
- 
+
 struct Cell
 {
-    Cell(std::optional<size_t> value = std::nullopt,
+    Cell(std::optional<uint> value = std::nullopt,
          bool isModifiable = true)
     : value{value}
     , isModifiable{isModifiable}
     {}
     friend std::ostream& operator<<(std::ostream& os, const Cell& cell)
     {
-        os << (cell.value ? *(cell.value) : static_cast<?>(' '));
+        os << (cell.value ? *(cell.value) : ' ');
         return os;
     }
     bool operator==(const Cell& rhs) const { return value == rhs.value; }
     bool operator<(const Cell& rhs) const { return value < rhs.value; }
+    operator bool() const { return static_cast<bool>(value); }
     void increment() { if(value) ++(*value); }
-    void changeValue(size_t new_value) { /*value.emplace(new_value);*/ value = (new_value); }
+    void changeValue(uint new_value) { value.emplace(new_value); }
  
-	std::optional<size_t> value;
+	std::optional<uint> value;
 	bool isModifiable;
 };
+
+namespace
+{
+    template<typename T>
+    std::vector<T> filterOutElementsWithoutValues(const std::vector<T>& vec)
+    {
+        auto hasValue = [](const T& element){ return element; };
+        using namespace boost::adaptors;
+        return boost::copy_range<std::vector<T>>(vec | filtered(hasValue));
+    }
+    
+    template<typename T>
+    bool hasUniqueElements(const std::vector<T>& vec)
+    {
+        auto temp = filterOutElementsWithoutValues(vec);
+        return temp.size() == std::set<T>(temp.begin(), temp.end()).size();
+    }
+    
+    uint charToUint(char ch) { return static_cast<uint>(ch-'0'); }
+}
  
 class SudokuSolver
 {
 public:
 	SudokuSolver(const std::string& sudokuGrid)
 	: actualIndex{0}
-	, sudokuVector(3, 3)//99
+	, sudokuVector(std::sqrt(sudokuGrid.size()), std::sqrt(sudokuGrid.size()))
 	{
 	    for(const auto& value : sudokuGrid)
 	    {
 	        sudokuVector.emplace_back(isdigit(value)
-	                                  ? Cell(static_cast<size_t>(value-'0'), false)
+	                                  ? Cell(charToUint(value), false)
 	                                  : Cell());
 	    }
 	}
 	void solve()
 	{
-		while(actualIndex < 9)//while(actualIndex < 81)
+	    while(true)
 		{
-		    std::cout << "*" << actualIndex << "*";
-		    //break;//remove this
-		    //std::cout <<"^^"<<isok()<<"^^";
-		    if(isok())
+////////////std::cout << "*" << actualIndex << "*";
+		    if(allCellsAreCorrect())
 		    {
-		        std::cout << "isok";
-		        //break;//remove this
+////////////////std::cout << "isok";
 		        goToNextModifiableCell();
-		        std::cout << "***" << actualIndex << "***";
+		        if(actualIndex >= sudokuVector.size()) break;
+////////////////std::cout << "***" << actualIndex << "***";
 		        setLowestPossibleValueInActualCell();
-		        std::cout << *actualCell().value;
-		        display();
-		        break;
 		    }
-		    else if(not isok() and
-		            actualCell().value and
-		            *actualCell().value < 9)
+		    else
 		    {
-		        std::cout<<"dwa";
-		        //break;//remove this
-		        incrementValueInActualCell();
-            }
-	        else
-		    {
-		        //break;//remove this
-		        std::cout << "trzy";
-		        cleanActualCell();
-		        goToPreviousModifiableCell();
+////////////////std::cout << "notok";
+                while(actualCell().value and
+                      *actualCell().value == 9)//magic number
+                {
+                    cleanActualCell();
+                    goToPreviousModifiableCell();
+                }
+                incrementValueInActualCell();
 		    }
-		    //break;
+		    display();
 		}
 	}
 	void display()
@@ -164,19 +139,43 @@ public:
 		sudokuVector.display();
 	}
 private:
-	size_t actualIndex;
+	uint actualIndex;
 	FlattenVector<Cell> sudokuVector;
+	
+	bool nthRowIsCorrect(uint row_index)
+	{
+	    auto nthRow = [this, row_index](uint index){ return index/sudokuVector.rows == row_index; };
+	    return hasUniqueElements(sudokuVector.getSpecificPart(nthRow));
+	}
+	
+	bool nthColIsCorrect(uint col_index)
+	{
+	    auto nthCol = [this, col_index](uint index){ return index%sudokuVector.cols == col_index; };
+	    return hasUniqueElements(sudokuVector.getSpecificPart(nthCol));
+	}
+	
+	bool nthSqrIsCorrect(uint sqr_index)
+	{
+	    auto nthSqr = [this, sqr_index](uint index)
+	    {
+	        auto row = index/sudokuVector.rows;
+	        auto col = index%sudokuVector.cols;
+	        return row == std::clamp(row, sqr_index/3, sqr_index/3+3-1) and
+	               col == std::clamp(col, sqr_index%3, sqr_index%3+3-1);   //magic numbers
+	    };
+	    return hasUniqueElements(sudokuVector.getSpecificPart(nthSqr));
+	}
+	
+	//add SudokuChecker
  
-    bool isok()//fix this
+    bool allCellsAreCorrect()
     {
-        for(auto i : boost::irange<size_t>(0, 3))//9
+        for(auto i : boost::irange(0u, sudokuVector.rows))
         {
-            std::cout <<""<<std::endl;
-            if(not (sudokuVector.checkNthRowCorrectness(i) and
-                    sudokuVector.checkNthColCorrectness(i) and
-                    sudokuVector.checkNthSqrCorrectness(i)))
+            if(not nthRowIsCorrect(i) or
+               not nthColIsCorrect(i) or
+               not nthSqrIsCorrect(i))
             {
-                std::cout <<"ddd";
                 return false;
             }
         }
@@ -185,21 +184,25 @@ private:
     Cell& actualCell() { return sudokuVector[actualIndex]; }
     const Cell& actualCell() const { return sudokuVector[actualIndex]; }
  
-    void goToNextModifiableCell() { while(not actualCell().isModifiable) { ++actualIndex; } }
-    void goToPreviousModifiableCell() { while(not actualCell().isModifiable) { --actualIndex; } }
+    void goToNextModifiableCell() { do { ++actualIndex; } while(not actualCell().isModifiable); }
+    void goToPreviousModifiableCell() { do { --actualIndex; } while(not actualCell().isModifiable); }
     void cleanActualCell() { actualCell() = Cell(); }
-    //void setLowestPossibleValueInActualCell() { actualCell() = Cell(1); }
-    void setLowestPossibleValueInActualCell() { actualCell().changeValue(1); }//sudokuVector[actualIndex].value.emplace('5'); std::cout << "$$"<<actualIndex<<sudokuVector[4]; }
+    void setLowestPossibleValueInActualCell() { actualCell().changeValue(1); }//magic number
     void incrementValueInActualCell() { actualCell().increment(); }
 };
  
 int main()
 {
-    const std::string sudokuGrid{"123"
-                                 "4 6"
-                                 "789"};
+    const std::string sudokuGrid{"7  2 4 1 "
+                                 "95  6  3 "
+                                 "2  1 8 57"
+                                 "  3  5   "
+                                 "    7    "
+                                 "1 23 9   "
+                                 "         "
+                                 "         "
+                                 "         "};
 	SudokuSolver sudokuSolver(sudokuGrid);
-	sudokuSolver.display();
 	sudokuSolver.solve();
 	sudokuSolver.display();
 	return 0;
